f92f8bd6b04c8b5c7326d702c4853e0a934bb8e3
cmotc <cmotc@openmailbox.org>
2014-12-15 02:38:57 -0500

    figuring out how to build slirp for android with NDK

--- update-slirp.sh
+++ update-slirp.sh
@@ -3,4 +3,13 @@ rm -rf jni && mkdir -p jni/
 wget http://ftp.de.debian.org/debian/pool/main/s/slirp/slirp_1.0.17.orig.tar.gz -O slirp.tar.gz && tar -zxvf slirp.tar.gz -C jni/
 cd jni && mv slirp-* slirp && cd ../
 rm slirp.tar.gz
-cd jni/slirp/src && ./configure && make
+export NDK="$HOME/.bin/android-ndk-r10d"
+export NDK_TOOLCHAIN=${NDK}/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+export NDK_SYSROOT=${NDK}/platforms/android-9/arch-arm
+cd jni/slirp/src 
+./configure 
+make CC=${NDK_TOOLCHAIN}gcc CFLAGS=--sysroot=${NDK_SYSROOT}
+echo ${NDK_TOOLCHAIN}gcc 
+ls ${NDK_TOOLCHAIN}gcc 
+echo ${NDK_SYSROOT}
+ls ${NDK_SYSROOT}
