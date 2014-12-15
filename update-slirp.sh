9b1445c585c18b4a12da96561841fac8c7a65a7d
cmotc <cmotc@openmailbox.org>
2014-12-15 03:23:59 -0500

    auto-update

--- update-slirp.sh
+++ update-slirp.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
 rm -rf jni/slirp/docs/ jni/slirp/src && rm jni/* && mkdir -p jni/
 mv jni/slirp jni/bakslirp && mv -f jni/bakslirp/.git jni/slirp/.git && rm -rf jni/bakslirp
-pwd
 cd jni/slirp && git checkout debian && git add . && git commit -am "auto-update" && git push origin debian && cd ../..
 wget http://ftp.de.debian.org/debian/pool/main/s/slirp/slirp_1.0.17.orig.tar.gz -O slirp.tar.gz && tar -zxvf slirp.tar.gz -C jni/
-pwd
 cd jni && mv slirp-* slirp && cd ../
+pwd
 rm slirp.tar.gz
 export NDK="$HOME/.bin/android-ndk-r10d"
 export NDK_TOOLCHAIN=${NDK}/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
