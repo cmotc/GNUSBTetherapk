e58b88ec84b83c200e963bb71975228f527884f7
cmotc <cmotc@openmailbox.org>
2014-12-15 03:15:16 -0500

    auto-update

--- update-slirp.sh
+++ update-slirp.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 rm -rf jni/slirp/docs/ jni/slirp/src && rm jni/* && mkdir -p jni/
-mv jni/slirp jni/bakslirp && mv -f jni/bakslirp/.git jni/slirp/.git
-cd jni/slirp && git add . && git commit -am "auto-update" && git push origin master && ..
+mv jni/slirp jni/bakslirp && mv -f jni/bakslirp/.git jni/slirp/.git && rm -rf jni/bakslirp
+cd jni/slirp && git add . && git commit -am "auto-update" && git push origin master && ../..
 wget http://ftp.de.debian.org/debian/pool/main/s/slirp/slirp_1.0.17.orig.tar.gz -O slirp.tar.gz && tar -zxvf slirp.tar.gz -C jni/
 cd jni && mv slirp-* slirp && cd ../
 rm slirp.tar.gz
