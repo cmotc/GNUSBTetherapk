503fa345be08019113f3002678b6448aaa781e7a
cmotc <cmotc@openmailbox.org>
2014-12-15 03:43:00 -0500

    auto-update

--- update-slirp.sh
+++ update-slirp.sh
@@ -5,9 +5,8 @@ rm -rf jni/bakslirp/docs/ jni/bakslirp/src && rm jni/backslirp/*
 wget http://ftp.de.debian.org/debian/pool/main/s/slirp/slirp_1.0.17.orig.tar.gz -O slirp.tar.gz && tar -zxvf slirp.tar.gz -C jni/
 cd jni && mv slirp-* slirp && cd ../
 cd jni/slirp && git checkout debian && git add . && git commit -am "auto-update" && git push origin debian && cd ../..
-mv -f jni/bakslirp/.git jni/slirp/.git && 
+mv -f jni/bakslirp/.git jni/slirp/.git
 rm -rf jni/bakslirp
-pwd
 rm slirp.tar.gz
 export NDK="$HOME/.bin/android-ndk-r10d"
 export NDK_TOOLCHAIN=${NDK}/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
