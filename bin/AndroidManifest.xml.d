81bbc6a861dffd43faf11392e1d62d38bc76ea36
cmotc <cmotc@openmailbox.org>
2014-12-16 01:20:56 -0500

    added strings and edited startSLiRP

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/dyngar-m/Projects/PersonalROM-schS738c/GNUSBTetherapk/bin/AndroidManifest.xml : \
-/home/dyngar-m/Projects/PersonalROM-schS738c/GNUSBTetherapk/AndroidManifest.xml \
--- gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/dyngar-m/Projects/PersonalROM-schS738c/GNUSBTetherapk/gen/com/cmotc/gnusbtether/R.java \
- : /home/dyngar-m/Projects/PersonalROM-schS738c/GNUSBTetherapk/res/layout/main.xml \
-/home/dyngar-m/Projects/PersonalROM-schS738c/GNUSBTetherapk/res/values/strings.xml \
-/home/dyngar-m/Projects/PersonalROM-schS738c/GNUSBTetherapk/bin/AndroidManifest.xml \
